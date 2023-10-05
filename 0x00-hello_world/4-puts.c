bfbba9e2e0808a0e64ff44c16eead1774cbff542
zezo773 <zezo77328@gmail.com>
2023-10-05 11:03:23 -0700

    ziad

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: A c program prints with puts function
+ * return: always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

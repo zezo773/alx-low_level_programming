62e8f655733fdf775046bf0e42aafd9e749ea612
zezo773 <zezo77328@gmail.com>
2023-10-10 14:05:34 -0700

    asd

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 
 /**
  * _isalpha - checks if character is a letter
- * both lowercase or uppercase 
+ *both lowercase or uppercase
  *
  * @c: takes input from other functions.
  *
  * Return: 1 is c if true else 0
- * */
+ */
 
 int _isalpha(int c)
 {

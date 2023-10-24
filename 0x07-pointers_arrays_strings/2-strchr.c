85202d721fd4423a734e4fe7a2981b3d491e9304
zezo773 <zezo77328@gmail.com>
2023-10-24 07:30:54 -0700

    as

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,18 @@
 #include "main.h"
 /**
- * _strchr - the first first occurrence of
- * the character C in the string
- * @s: the string
- * @c:the character
- * Return: a pointer for the c character
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
  */
-
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i = 0;
 
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
-		;
-	if (s[i] == c)
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
 		return (s + i);
-	else
-		return (NULL);
+	return ('\0');
 }

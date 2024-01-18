79c5f157289b70e477b0102de2d400ea78db4503
zezo773 <zezo77328@gmail.com>
2024-01-17 23:29:15 -0800

    asf

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD =/tmp/nrandom.so
+wget -P /tmp/ https://raw.github.com/zezo773/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD =/tmp/libgiga.so

72c4fae3496602901c506954a54c39215edd5c62
zezo773 <zezo77328@gmail.com>
2024-03-22 06:53:12 -0700

    safd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - The Entry Point
+ *
+ * Description: program that prints exactly "Programming is like
+ * building a multilingual puzzle ollowed by a new line
+ *
+ * Return: (0)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

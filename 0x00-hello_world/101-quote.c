6b0c7141dc20cd1e69e1fd57b0520ec581922bdd
zezo773 <zezo77328@gmail.com>
2024-03-22 07:44:19 -0700

    asdf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - The Entry Point
+ *
+ * a C program that prints exactly and that piece of art is useful
+ * " - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
+ * Return: (1)
+ */
+
+int main(void)
+{
+	char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, qu, 59);
+	return (1);
+}

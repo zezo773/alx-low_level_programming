2acd61f5dd466a048dfc0fa1ef33ddd881c11901
zezo773 <zezo77328@gmail.com>
2024-03-23 04:53:43 -0700

    sadf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - The Entry Point
+ *
+ * program will assign a random number to the
+ * variable n each time it is executed.
+ * Complete the source code in order to print the last digit of
+ * the number stored in the variable n.
+ *  Return: (0)
+ */
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 
 	if ((n % 10) > 5)
 	{

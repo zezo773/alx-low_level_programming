898907c546e7ef456429cacda09f0813b8802afa
zezo773 <zezo77328@gmail.com>
2024-03-24 11:49:19 -0700

    asdf

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point
+ *
+ * a program that prints all single
+ * digit numbers of base 10 starting from 0,
+ * followed by a new line.
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}

9286449aecee4429749454f4609e6b5358a8c8eb
zezo773 <zezo77328@gmail.com>
2024-03-25 02:54:07 -0700

    ads

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - The Entry Point
  *
- * a program that prints all single digit numbers 
+ * a program that prints all single digit numbers
  * of base 10 starting from 0,
  * followed by a new line.
  *

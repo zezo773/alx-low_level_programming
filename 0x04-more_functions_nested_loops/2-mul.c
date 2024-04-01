03001c1481a946b11eeeaf8b4ca0d18678fd51b2
zezo773 <zezo77328@gmail.com>
2024-03-31 22:33:38 -0700

    asdf

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - The Entry Point
+ *
+ * a function that multiplies two integers.
+ *
+ * @a: Is A First Parametter
+ *
+ * @b: Is A Second Parametter
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

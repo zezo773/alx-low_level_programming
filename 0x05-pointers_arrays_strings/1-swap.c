b225fc2ea745797aa631c9c05d9996acd322bdf3
zezo773 <zezo77328@gmail.com>
2024-04-02 06:04:14 -0700

    asdf

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - The Entry Point
+ *
+ * a function that swaps the values of two integers.
+ *
+ * @a: The First Parametter
+ * @b: The Second Parametter
+ *
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

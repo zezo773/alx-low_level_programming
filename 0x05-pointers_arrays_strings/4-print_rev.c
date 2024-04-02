cd0acfd0d68040dbbbbf111e7476a5c5d7b7f573
zezo773 <zezo77328@gmail.com>
2024-04-02 07:41:30 -0700

    asdf

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,19 +12,21 @@
 
 void print_rev(char *s)
 {
-	int i, len, n;
+	int i, j, len;
 
 	i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	len = i;
-	n = len;
-	while (n >= 0)
+
+	for (j = len - 1; j >= 0; j--)
 	{
-		_putchar(s[n]);
-		n--;
+		_putchar(s[j]);
 	}
+
 	_putchar('\n');
 }

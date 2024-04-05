b584f8ff9931bf81db22fd3b3ed0d86e3d11aec0
zezo773 <zezo77328@gmail.com>
2024-04-05 05:04:06 -0700

    sfda

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,31 +1,35 @@
 #include "main.h"
 
 /**
- * _strspn - The Entry Point
+ * *_strspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match in s
  *
- * a function that gets the length of a prefix substring.
- *
- * @s: The First Parametter
- * @accept: The Second Parametter
- *
- * Return: unsigned int
+ * Return: the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
+	int i, j, f, flag;
 
-	for (i = 0; *s != '\0'; i++)
+	f = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
+		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == accept[j])
+			if (s[i] == accept[j])
 			{
-				return (s);
+				f++;
+				flag = 1;
 			}
 		}
-		s++;
+		if (flag == 0)
+		{
+			return (f);
+		}
 	}
 
-	return (NULL);
+	return (0);
 }

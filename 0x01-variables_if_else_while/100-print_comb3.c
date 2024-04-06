6697947c892bec7177549f292aeeb45759b7b509
zezo773 <zezo77328@gmail.com>
2024-04-06 06:08:42 -0700

    asdf

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry Point
+ *
+ * a program that prints all possible
+ * different combinations of two digits.
+ *
+ * Return: (0)*/
+
+int main(void)
+{
+	
+	return (0);
+}

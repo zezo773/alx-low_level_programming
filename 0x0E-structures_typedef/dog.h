53170dcb7abc2f0650cd823d117984d37d502e18
zezo773 <zezo77328@gmail.com>
2024-04-29 01:11:18 -0700

    asd

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _HEADER_
+#define _HEADER_
 
-/* struct dog - a new type
- * @name: First member
- * @age: Second member
- * @owner: Third member
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Description: Define a new type struct dog
+ * Description: The attributes of a dog.
  */
 struct dog
 {
@@ -16,7 +17,7 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
+ * dog_t - Typedef for dog structure
  */
 typedef struct dog dog_t;
 
@@ -24,4 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif

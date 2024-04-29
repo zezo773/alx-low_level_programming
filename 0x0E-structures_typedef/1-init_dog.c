949a85f1440c92aef4f1a83086110d5a6d6726fc
zezo773 <zezo77328@gmail.com>
2024-04-29 01:01:05 -0700

    asfd

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stddef.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -18,7 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		(*d).name = name;
+		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}

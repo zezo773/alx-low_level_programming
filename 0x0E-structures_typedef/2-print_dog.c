9fc7934376df6cb6a389a9b3ebd72c1a912a251a
zezo773 <zezo77328@gmail.com>
2024-04-29 01:40:26 -0700

    asdf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
 
-/***/
+/**
+ * print_dog - The Entry Point
+ *
+ * a function that prints a struct dog
+ *
+ * @d: The Pametter
+ *
+ * return: void
+ */
 
 void print_dog(struct dog *d)
 {

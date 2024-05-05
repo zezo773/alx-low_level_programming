41baab23d574a776dd6c4755729f20f5aea09582
zezo773 <zezo77328@gmail.com>
2024-05-05 07:56:24 -0700

    saf

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+  * str_concat - function that concatenates two strings.
+  * @s1: The string to be concatenated.
+  * @s2: The string to be concatenated.
+  * Return: concatenates two strings.
+  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int count, len = 0, concat = 0;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	count = 0;
+	while (s1[count] || s2[count])
+	{
+		count++;
+		len++;
+	}
+
+	ptr = malloc(sizeof(char) * len);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	count = 0;
+	while (s1[count])
+	{
+		ptr[concat++] = s1[count];
+		count++;
+	}
+	while (s2[count])
+	{
+		ptr[concat++] = s2[count];
+		count++;
+	}
+
+	return (ptr);
+}

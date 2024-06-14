3aa42d1e7fff3bab16258be2af01d2c8590442b7
zezo773 <zezo77328@gmail.com>
2024-06-14 04:51:32 -0700

    sadf

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,7 @@
  *
  * Return: numbers of nodes.
  */
-size_t print_listint(const listint_t *h)
+/**size_t print_listint(const listint_t *h)
 {
 	size_t nnodes = 0;
 
@@ -17,4 +17,17 @@ size_t print_listint(const listint_t *h)
 		nnodes++;
 	}
 	return (nnodes);
+}
+*/
+size_t print_listint(const listint_t *h)
+{
+        size_t count = 0;
+
+        while (h != NULL)
+        {
+                count++;
+                printf("%d\n", h->n);
+                h = h->next;
+        }
+        return (count);
 }

bb99b53bf739f929508b57002c5a7bcfb89d3efb
zezo773 <zezo77328@gmail.com>
2024-06-26 01:04:03 -0700

    safd

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,21 +9,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-
-	int counter = 0;
+	size_t count = 0;
 
 	if (h == NULL)
-		return (counter);
-
-	while (h->prev != NULL)
-		h = h->prev;
+		return (count);
 
 	while (h != NULL)
 	{
+		count++;
 		printf("%d\n", h->n);
 		h = h->next;
-		counter++;
 	}
-
-	return (counter);
+	return (count);
 }

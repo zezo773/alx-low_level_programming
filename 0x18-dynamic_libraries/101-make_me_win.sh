2c94d662d195bdde7ae7ba9ae63587b844d6272a
zezo773 <zezo77328@gmail.com>
2024-06-29 06:31:04 -0700

    sadf

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/zezo773/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so
-export LD_PRELOAD=/tmp/
+wget -P /tmp https://github.com/zezo773/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so 

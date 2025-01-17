dfb21daab13cbdf58086b05644f1a107f52c4f9e
zezo773 <zezo77328@gmail.com>
2025-01-17 17:48:25 +0200

    all tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Return: 0 always (Success)
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
